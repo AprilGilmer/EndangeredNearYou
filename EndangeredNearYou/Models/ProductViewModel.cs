e06853337aeb660cf0d3922c32956a21b3db8a53
April Gilmer <aprilgilmer1@gmail.com>
2025-09-09 23:05:43 -0500

    Got Web project running again

--- EndangeredNearYou/Models/ProductViewModel.cs
+++ EndangeredNearYou/Models/ProductViewModel.cs
@@ -1,12 +1,16 @@
-﻿namespace EndangeredNearYou.Web.Models
+﻿using EndangeredNearYou.Domain.Entities;
+
+namespace EndangeredNearYou.Web.Models
 {
     public class ProductViewModel
     {
+        public int ProductId { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
         public int OnSale { get; set; }
         public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
     }
 }
